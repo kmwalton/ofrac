83a1aa6dadee939fd1815665f51cdf0d157cbac8
kmwalton <ken.walton@gmail.com>
2021-07-06 12:27:43 -0400

    New executable to convert any DFN to tecplot format

---
+++ ofracs-to-tecplot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Convert some orthogonal fracture network to tecplot format."""
+
+import sys
+import ofracs
+
+import argparse
+
+
+if __name__ == '__main__':
+
+    argp = argparse.ArgumentParser()
+
+    argp.add_argument('FILE_IN',
+        help='input file name',
+        )
+
+    argp.add_argument( 'FILE_OUT',
+        nargs='?',
+        default='-',
+        help="Filename for output (or stdout if blank or '-')",
+        )
+
+    args = argp.parse_args()
+
+    g = ofracs.parse(args.FILE_IN)
+
+    if not args.FILE_OUT or args.FILE_OUT == '-':
+        g.printTecplot()
+    else:
+        with open(args.FILE_OUT,'w') as fout:
+            g.printTecplot(fout=fout)
+            
