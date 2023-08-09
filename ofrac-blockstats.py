12c9ba58fef31938450c6839c6ba1e1e7d1fdf98
kmwalton <56695898+kmwalton@users.noreply.github.com>
2023-08-09 12:06:18 -0400

    Refactor to class-ify MatrixBlock infrastructure
    Classes now available for MatrixBlockOFracGrid and MatrixBlock for import & use by arbitrary clients --- no need to import the ofrac-blockstats.py script to access these.

--- ofrac-blockstats.py
+++ ofrac-blockstats.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 
 import os
-import datetime
 import argparse
 import tempfile
 import subprocess
 import shutil
-import warnings
-from re import sub
-from math import floor,ceil
-from operator import attrgetter, mul
-from itertools import combinations, product, cycle
-from functools import reduce
 
 import numpy as np
-import scipy
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 import ofracs
+from ofrac.matrixblock import MatrixBlockOFracGrid, MatrixBlock
 
 import logging
 logger = logging.getLogger(__file__)
@@ -37,332 +28,7 @@ def process_pstats(dfn):
         ofracs.OFracGrid.pickleTo(dfn, f)
         subprocess.run([pcalc, f.name,])
 
-class _SortedDFN(ofracs.OFracGrid):
 
-    def __init__(self, ofracgrid_obj):
-        self.__dict__.update(ofracgrid_obj.__dict__)
-
-        nperpax = self.getFxCounts()
-        obinfx = list(
-                -np.ones(nperpax[i], dtype=int) for i in range(3))
-        locfx = list(
-                np.full(nperpax[i], np.inf, dtype=np.single)
-                    for i in range(3))
-        nextind = 3*[0,]
-
-        # make bins for each fracture orientation;
-        # classify fractures
-        for ifrac,frac in enumerate(self.iterFracs()):
-            (perpaxis, axisval) = frac.determinePerpAxisVal()
-            iunsorted = nextind[perpaxis]
-            obinfx[perpaxis][iunsorted] = ifrac
-            locfx[perpaxis][iunsorted] = axisval
-            nextind[perpaxis] += 1
-
-        logger.debug(
-            f'Put {nextind} fractures into yz, xz, xy orientation bins')
-        logger.debug('Sorting bins...')
-
-        self.sorted_fx = 3*[None,]
-        self.sorted_fx_locs = 3*[None,]
-        for iax in range(3):
-            isorted = np.argsort(locfx[iax])
-            self.sorted_fx[iax] = obinfx[iax][isorted]
-            self.sorted_fx_locs[iax] = locfx[iax][isorted]
-
-        logger.debug('Sorting bins...done.')
-
-    def find_block_bounds(self, pt):
-        """Find bounding box locations and bounding fracture indices"""
-
-        retbnds = np.zeros(6, dtype=np.single)
-        retbnds[::2] = dfn.domainOrigin
-        retbnds[1::2] = retbnds[::2] + np.array(dfn.domainSize, dtype=np.single)
-
-        retifx = -np.ones(6, dtype=np.int32)
-
-        def _search(iax, pt, sortedifx):
-
-            # do linear search of perpendicular fractures to find intercept
-            for ifx in sortedifx:
-                
-                # fracture bounding box
-                fxbb = self._fx[ifx].d
-
-                # examine coordinates in direction
-                iscandidate = True
-                for iiax in range(3):
-                    if iiax != iax:
-                        inbb = (fxbb[2*iiax] <= pt[iiax] <= fxbb[2*iiax+1])
-                        #breakpoint()
-                        logger.debug(
-                          f'Fx#{ifx:2d}\'s {"xyz"[iiax]} bounds: '
-                          +f'{fxbb[2*iiax]:7.3f} <= '
-                          +f'{pt[iiax]:7.3f} <= {fxbb[2*iiax+1]:7.3f}? '
-                          + str(inbb))
-                        iscandidate &= inbb
-
-                if iscandidate:
-                    logger.debug(f'Found intercept along {"xyz"[iax]} of '
-                            + f'{pt} with {self._fx[ifx]}')
-                    #breakpoint()
-                    return fxbb[2*iax], ifx
-
-            return None
-
-        def _searchup(iax, pt, sortedifx):
-            ret = _search(iax, pt, sortedifx)
-            if ret is None:
-                return float(self.domainOrigin[iax]+self.domainSize[iax]), -1,
-            return ret
-
-        def _searchdown(iax, pt, ifx, ileft):
-            ret = _search(iax, pt, np.flip(ifx[:ileft]))
-            if ret is None:
-                return float(self.domainOrigin[iax]), -1,
-            return ret
-
-        for iax,vax in enumerate(pt):
-            ileft = np.searchsorted(self.sorted_fx_locs[iax], vax)
-
-            (_l, _ifx) = _searchdown(iax, pt, self.sorted_fx[iax], ileft)
-            retbnds[2*iax  ]=_l
-            retifx[2*iax  ]=_ifx
-
-            (_l, _ifx) = _searchup(iax, pt, self.sorted_fx[iax][ileft:])
-            retbnds[2*iax+1]=_l
-            retifx[2*iax+1]=_ifx
-            
-        return (retbnds, retifx)
-
-        
-
-
-class _MatrixBlock:
-
-    def __init__(self, dfn, pt):
-        """Analyze the dfn to find the block around `pt`"""
-
-        self.pt = pt
-        """[x y z]"""
-
-        #breakpoint()
-        (_bb, _bfx) = dfn.find_block_bounds(pt)
-        #self.bb = _MatrixBlock._dummy_block_bounds(pt, 2.)
-        self.bb = _bb
-        """[x1 x2 y1 y2 z1 z2]"""
-
-        _nbfx = sum(ifx > -1 for ifx in _bfx)
-        self.bfx = sorted(np.fromiter(filter(lambda ifx: ifx>-1, _bfx), count=_nbfx,
-                dtype=np.int32))
-        """Sorted list of indicies of bounding fractures"""
-
-        _length = _bb[1::2]-_bb[::2]
-        self.L = _length
-        self.V = np.product(_length)
-        self.A = 2*sum(np.product(a) for a in combinations(_length, 2))
-        self.Afrac = 0.
-        self.axy = _length[1]/_length[0]
-        self.axz = _length[2]/_length[0]
-
-    def iter_pts(self):
-        _tmp = np.zeros(3)
-        for x in self.bb[:2]:
-            _tmp[0] = x
-            for y in self.bb[2:4]:
-                _tmp[1] = y
-                for z in self.bb[4:]:
-                    _tmp[2] = z
-                    yield _tmp
-
-    @staticmethod
-    def pts_to_tecplot(bllist, fn):
-
-        tecout = ''
-        tecout += f'# {os.path.realpath(__file__)} on {datetime.datetime.now()}\n'
-        tecout += 'VARIABLES="X","Y","Z"\n'
-        tecout += '\n'
-
-        tecout += f'ZONE T="Block initial points" '
-        tecout += f'ZONETYPE=ORDERED I={len(bllist)} DATAPACKING=POINT'
-        tecout += '\n'
-
-        for bl in bllist:
-            tecout += ' '.join(f'{v:8.3f}' for v in bl.pt) + '\n'
-
-        with open(fn, 'w') as fout:
-            print(tecout, file=fout)
-
-        logger.info(f'Wrote sample points as Tecplot data in {fn}')
-
-    @staticmethod
-    def blocks_to_tecplot(bllist, fn):
-        tecout = ''
-        tecout += f'# {os.path.realpath(__file__)} on {datetime.datetime.now()}\n'
-        tecout += 'VARIABLES="X","Y","Z","V","A","X:Y aspect","X:Z aspect","Frac SA"\n'
-        tecout += '\n'
-
-        ne = len(bllist)
-        tecout += f'ZONE T="Block data" '
-        tecout += f'ZONETYPE=FEBRICK '
-        tecout += f'NODES={8*ne} ELEMENTS={ne} DATAPACKING=BLOCK '
-        tecout += 'VARLOCATION=([4-8]=CELLCENTERED)'
-        tecout += '\n'
-
-        # X,Y,Z
-        for i in range(3):
-            tecout += f'# {"XYZ"[i]}\n'
-            for bl in bllist:
-                tecout += ' '.join(f'{v[i]:.3f}' for v in bl.iter_pts()) + '\n'
-        # values
-        for v in ['V', 'A', 'axy', 'axz', 'Afrac',]:
-            tecout += '# '+v+'\n'
-            tecout += '\n'.join(str(getattr(bl, v)) for bl in bllist) + '\n'
-
-        # connectivity
-        for ibl in range(len(bllist)):
-            i = 8*ibl
-            tecout += ' '.join(str(i+ii) for ii in [1,5,7,3,2,6,8,4])+'\n'
-
-
-        with open(fn, 'w') as fout:
-            print(tecout, file=fout)
-
-        logger.info(f'Wrote blocks as Tecplot data in {fn}')
-
-    @staticmethod
-    def _dummy_block_bounds(pt, s):
-        bb = np.zeros(6, dtype=np.single)
-        bb[0] = pt[0]-s
-        bb[1] = pt[0]+s
-        bb[2] = pt[1]-s
-        bb[3] = pt[1]+s
-        bb[4] = pt[2]-s
-        bb[5] = pt[2]+s
-        return bb
-
-def process_blocks_regular(dfn):
-    """Find blocks using a grid, centred at halfway points in the dfn's grid"""
-
-    domo = np.array(dfn.domainOrigin, dtype=np.single)
-    doms = np.array(dfn.domainSize, dtype=np.single)
-
-
-    gl = [ np.fromiter(dfn.iterGridLines(ax), count=nax, dtype=np.single)
-            for ax, nax in zip(range(3), dfn.getGridLineCounts()) ]
-    sample_grid = 3*[None,]
-    for i in range(3):
-        if gl[i] is None:
-            sample_grid[i] = np.zeros((1,))
-        else:
-            sample_grid[i] = (gl[i][1:] + gl[i][:-1])/2
-    del gl
-
-    blocks = {}
-    for p in product(*sample_grid):
-        logger.debug(f'Evaluating grid point {p}')
-        bl = _MatrixBlock(dfn, p)
-        blbfx = tuple(bl.bfx)
-        if blbfx not in blocks:
-            blocks[blbfx] = bl
-            logger.debug(f'Adding block at {p} with bounding fractures {blbfx}.')
-        else:
-            logger.debug(f'Rejecting block at {p}')
-
-    npts = reduce(mul, map(attrgetter('size'), sample_grid))
-    logger.info(f'Processed {npts} matrix blocks; using {len(blocks)}.')
-
-    return list(blocks.values())
-
-def process_blocks_random(dfn, npts=5):
-
-    domo = np.array(dfn.domainOrigin, dtype=np.single)
-    doms = np.array(dfn.domainSize, dtype=np.single)
-
-    blocks = []
-
-    for i in range(npts):
-        p = domo+doms*np.random.rand(3)
-        logger.debug(f'Chose point {p}')
-
-        blocks.append(_MatrixBlock(dfn, p))
-
-    logger.info(f'Processed {npts} matrix blocks')
-    return blocks
-
-def histograms_to_png(block_list, filename_prefix):
-
-    def _make_plot(v, name, xlab, pfx, **kwargs):
-        fig, axes = plt.subplots(1)
-        plt.title(f'Histogram of Matrix Block {name}')
-        plt.xlabel(xlab)
-        plt.ylabel(f'Frequency')
-
-        hist = plt.hist(v, rwidth=0.75)
-        _mi,_ma = plt.xlim()
-        plt.xlim(left=floor(_mi), right=ceil(_ma))
-
-        if 'vbars' in kwargs:
-            lines = ["-","--","-.",":"]
-            linecycler = cycle(lines)
-
-            vlines = []
-            for labl,val in kwargs['vbars']:
-                ll = labl+f' = {val:.2f}'
-                vlines.append(
-                    plt.axvline(
-                        x=val,
-                        label=ll,
-                        color='black',
-                        linestyle=next(linecycler)
-                ))
-
-            plt.legend(handles=vlines)
-
-        fn = f'{filename_prefix}_{sub(" ","_",name)}.png'
-        plt.savefig(fn)
-        logger.info(f'Saved {name} histogram as {fn}')
-        plt.clf()
-    
-    _nbl = len(block_list)
-
-    # Volume
-    v = np.fromiter(map(attrgetter('V'), block_list), count=_nbl,
-            dtype=np.single)
-    stats = scipy.stats.describe(v)
-    _make_plot(v, 'Volume', 'Volume [$m^3$]', filename_prefix,
-            vbars=[
-            ('$\mu_{geo}$',scipy.stats.gmean(v),),
-            ('$\mu$',stats.mean,),
-            ])
-    
-
-    # Aspect
-    #with warnings.simplefilter('ignore'):
-    v = np.fromiter(map(attrgetter('axy'), block_list), count=_nbl,
-        dtype=np.single)
-    v = np.log10(v)
-    stats = scipy.stats.describe(v)
-    _make_plot(v, 'Aspect Ratio', '$log_{10}$(Aspect) [-]', filename_prefix,
-            vbars=[('$\mu$',stats.mean,),])
-
-    # x-Length
-    # y-Length
-    v = np.zeros((_nbl,3),np.single)
-    for r in range(_nbl):
-        v[r,:] = block_list[r].L
-    _make_plot(v[:,0], 'x-Length', 'x-Length [$m$]', filename_prefix,
-        vbars=[
-            ('$\mu_{geo}$',scipy.stats.gmean(v[:,0]),),
-            ('$\mu$',np.mean(v[:,0]),),
-            ('median',np.median(v[:,0]),),
-        ])
-    _make_plot(v[:,1], 'y-Length', 'y-Length [$m$]', filename_prefix,
-        vbars=[
-            ('$\mu_{geo}$',scipy.stats.gmean(v[:,0]),),
-            ('$\mu$',np.mean(v[:,1]),),
-            ('median',np.median(v[:,0]),),
-        ])
 
 
 _FILTERS = {
@@ -432,6 +98,7 @@ if __name__ == '__main__':
         np.random.seed(int(args.random_seed))
 
     # process input dfn and modifications to it
+    ofracs.__FX_COLLAPSE_POLICY__ = 'omit'
     dfn = ofracs.parse(args.FILE)
     if args.sub_domain is not None:
         try:
@@ -445,7 +112,7 @@ if __name__ == '__main__':
         dfn.nudgeAll(args.nudge)
 
     # convert DFN to _SortedDFN for processing
-    dfn = _SortedDFN(dfn)
+    dfn = MatrixBlockOFracGrid(dfn)
 
     # argument fixup
     try:
@@ -458,10 +125,10 @@ if __name__ == '__main__':
     # determine sample of matrix blocks
     blocks = None
     if isinstance(args.nblocks, int):
-        blocks = process_blocks_random(dfn, args.nblocks)
+        blocks = dfn.find_blocks_random(args.nblocks)
     elif isinstance(args.nblocks, str) and \
             args.nblocks.lower()[:3]=='reg':
-        blocks = process_blocks_regular(dfn)
+        blocks = dfn.find_blocks_regular_grid()
     else:
         argp.error('Invalid value for --nblocks')
 
@@ -470,7 +137,8 @@ if __name__ == '__main__':
         nprev = len(blocks)
         blocks = list(filter(_FILTERS[args.block_filter], blocks))
         logger.info(
-          f'Filter "{args.block_filter}" removed {nprev-len(blocks)} blocks.')
+          f'Filter "{args.block_filter}" removed {nprev-len(blocks)} blocks;'
+          +f' {len(blocks)} remain.')
 
 
     # output to speed up next computation:
@@ -487,10 +155,10 @@ if __name__ == '__main__':
 
     # write points datafile
     #process_pstats(dfn)
-    _MatrixBlock.pts_to_tecplot(blocks, args.prefix+'_points.dat')
-    _MatrixBlock.blocks_to_tecplot(blocks, args.prefix+'_blocks.dat')
+    MatrixBlock.pts_to_tecplot(blocks, args.prefix+'_points.dat')
+    MatrixBlock.blocks_to_tecplot(blocks, args.prefix+'_blocks.dat')
 
-    histograms_to_png(blocks, args.prefix)
+    dfn.to_histograms_pngs(blocks, args.prefix)
 
     if args.lay_to_jpg:
         try:
---
+++ ofrac/matrixblock.py
@@ -0,0 +1,349 @@
+"""Support for determining matrix blocks in an OFracGrid"""
+
+import os
+import datetime
+import warnings
+from re import sub
+from math import floor,ceil
+from operator import attrgetter, mul
+from itertools import combinations, product, cycle
+from functools import reduce
+
+import scipy
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import ofracs
+
+import logging
+logger = logging.getLogger(__name__)
+
+class MatrixBlockOFracGrid(ofracs.OFracGrid):
+
+    def __init__(self, ofracgrid_obj):
+        self.__dict__.update(ofracgrid_obj.__dict__)
+
+        nperpax = self.getFxCounts()
+        obinfx = list(
+                -np.ones(nperpax[i], dtype=int) for i in range(3))
+        locfx = list(
+                np.full(nperpax[i], np.inf, dtype=np.single)
+                    for i in range(3))
+        nextind = 3*[0,]
+
+        # make bins for each fracture orientation;
+        # classify fractures
+        for ifrac,frac in enumerate(self.iterFracs()):
+            (perpaxis, axisval) = frac.determinePerpAxisVal()
+            iunsorted = nextind[perpaxis]
+            obinfx[perpaxis][iunsorted] = ifrac
+            locfx[perpaxis][iunsorted] = axisval
+            nextind[perpaxis] += 1
+
+        logger.debug(
+            f'Put {nextind} fractures into yz, xz, xy orientation bins')
+        logger.debug('Sorting bins...')
+
+        self.sorted_fx = 3*[None,]
+        self.sorted_fx_locs = 3*[None,]
+        for iax in range(3):
+            isorted = np.argsort(locfx[iax])
+            self.sorted_fx[iax] = obinfx[iax][isorted]
+            self.sorted_fx_locs[iax] = locfx[iax][isorted]
+
+        logger.debug('Sorting bins...done.')
+
+    def find_block_bounds(self, pt):
+        """Find bounding box locations and bounding fracture indices"""
+
+        retbnds = np.zeros(6, dtype=np.single)
+        retbnds[::2] = self.domainOrigin
+        retbnds[1::2] = retbnds[::2] + np.array(self.domainSize, dtype=np.single)
+
+        retifx = -np.ones(6, dtype=np.int32)
+
+        def _search(iax, pt, sortedifx):
+
+            # do linear search of perpendicular fractures to find intercept
+            for ifx in sortedifx:
+                
+                # fracture bounding box
+                fxbb = self._fx[ifx].d
+
+                # examine coordinates in direction
+                iscandidate = True
+                for iiax in range(3):
+                    if iiax != iax:
+                        inbb = (fxbb[2*iiax] <= pt[iiax] <= fxbb[2*iiax+1])
+                        #breakpoint()
+                        logger.debug(
+                          f'Fx#{ifx:2d}\'s {"xyz"[iiax]} bounds: '
+                          +f'{fxbb[2*iiax]:7.3f} <= '
+                          +f'{pt[iiax]:7.3f} <= {fxbb[2*iiax+1]:7.3f}? '
+                          + str(inbb))
+                        iscandidate &= inbb
+
+                if iscandidate:
+                    logger.debug(f'Found intercept along {"xyz"[iax]} of '
+                            + f'{pt} with {self._fx[ifx]}')
+                    #breakpoint()
+                    return fxbb[2*iax], ifx
+
+            return None
+
+        def _searchup(iax, pt, sortedifx):
+            ret = _search(iax, pt, sortedifx)
+            if ret is None:
+                return float(self.domainOrigin[iax]+self.domainSize[iax]), -1,
+            return ret
+
+        def _searchdown(iax, pt, ifx, ileft):
+            ret = _search(iax, pt, np.flip(ifx[:ileft]))
+            if ret is None:
+                return float(self.domainOrigin[iax]), -1,
+            return ret
+
+        for iax,vax in enumerate(pt):
+            ileft = np.searchsorted(self.sorted_fx_locs[iax], vax)
+
+            (_l, _ifx) = _searchdown(iax, pt, self.sorted_fx[iax], ileft)
+            retbnds[2*iax  ]=_l
+            retifx[2*iax  ]=_ifx
+
+            (_l, _ifx) = _searchup(iax, pt, self.sorted_fx[iax][ileft:])
+            retbnds[2*iax+1]=_l
+            retifx[2*iax+1]=_ifx
+            
+        return (retbnds, retifx)
+
+    def find_blocks_regular_grid(self):
+        """Find blocks using a grid, centred at halfway points in the dfn's grid"""
+
+        domo = np.array(self.domainOrigin, dtype=np.single)
+        doms = np.array(self.domainSize, dtype=np.single)
+
+
+        gl = [ np.fromiter(self.iterGridLines(ax), count=nax, dtype=np.single)
+                for ax, nax in zip(range(3), self.getGridLineCounts()) ]
+        sample_grid = 3*[None,]
+        for i in range(3):
+            if gl[i] is None:
+                sample_grid[i] = np.zeros((1,))
+            else:
+                sample_grid[i] = (gl[i][1:] + gl[i][:-1])/2
+        del gl
+
+        blocks = {}
+        for p in product(*sample_grid):
+            logger.debug(f'Evaluating grid point {p}')
+            bl = MatrixBlock(self, p)
+            blbfx = tuple(bl.bfx)
+            if blbfx not in blocks:
+                blocks[blbfx] = bl
+                logger.debug(f'Adding block at {p} with bounding fractures {blbfx}.')
+            else:
+                logger.debug(f'Rejecting block at {p}')
+
+        npts = reduce(mul, map(attrgetter('size'), sample_grid))
+        logger.info(f'Processed {npts} matrix blocks; using {len(blocks)}.')
+
+        return list(blocks.values())
+
+    def find_blocks_random(self, npts=5):
+        """Return a list of "random" blocks"""
+
+        domo = np.array(self.domainOrigin, dtype=np.single)
+        doms = np.array(self.domainSize, dtype=np.single)
+
+        blocks = []
+
+        for i in range(npts):
+            p = domo+doms*np.random.rand(3)
+            logger.debug(f'Chose point {p}')
+
+            blocks.append(MatrixBlock(self, p))
+
+        logger.info(f'Processed {npts} matrix blocks')
+        return blocks
+
+    def to_histograms_pngs(self, block_list, filename_prefix):
+        """Create predefined histogram images"""
+
+        def _make_plot(v, name, xlab, pfx, **kwargs):
+            fig, axes = plt.subplots(1)
+            plt.title(f'Histogram of Matrix Block {name}')
+            plt.xlabel(xlab)
+            plt.ylabel(f'Frequency')
+
+            hist = plt.hist(v, rwidth=0.75)
+            _mi,_ma = plt.xlim()
+            plt.xlim(left=floor(_mi), right=ceil(_ma))
+
+            if 'vbars' in kwargs:
+                lines = ["-","--","-.",":"]
+                linecycler = cycle(lines)
+
+                vlines = []
+                for labl,val in kwargs['vbars']:
+                    ll = labl+f' = {val:.2f}'
+                    vlines.append(
+                        plt.axvline(
+                            x=val,
+                            label=ll,
+                            color='black',
+                            linestyle=next(linecycler)
+                    ))
+
+                plt.legend(handles=vlines)
+
+            fn = f'{filename_prefix}_{sub(" ","_",name)}.png'
+            plt.savefig(fn)
+            logger.info(f'Saved {name} histogram as {fn}')
+            plt.clf()
+        
+        _nbl = len(block_list)
+
+        # Volume
+        v = np.fromiter(map(attrgetter('V'), block_list), count=_nbl,
+                dtype=np.single)
+        stats = scipy.stats.describe(v)
+        _make_plot(v, 'Volume', 'Volume [$m^3$]', filename_prefix,
+                vbars=[
+                ('$\mu_{geo}$',scipy.stats.gmean(v),),
+                ('$\mu$',stats.mean,),
+                ])
+        
+
+        # Aspect
+        #with warnings.simplefilter('ignore'):
+        v = np.fromiter(map(attrgetter('axy'), block_list), count=_nbl,
+            dtype=np.single)
+        v = np.log10(v)
+        stats = scipy.stats.describe(v)
+        _make_plot(v, 'Aspect Ratio', '$log_{10}$(Aspect) [-]', filename_prefix,
+                vbars=[('$\mu$',stats.mean,),])
+
+        # x-Length
+        # y-Length
+        v = np.zeros((_nbl,3),np.single)
+        for r in range(_nbl):
+            v[r,:] = block_list[r].L
+        _make_plot(v[:,0], 'x-Length', 'x-Length [$m$]', filename_prefix,
+            vbars=[
+                ('$\mu_{geo}$',scipy.stats.gmean(v[:,0]),),
+                ('$\mu$',np.mean(v[:,0]),),
+                ('median',np.median(v[:,0]),),
+            ])
+        _make_plot(v[:,1], 'y-Length', 'y-Length [$m$]', filename_prefix,
+            vbars=[
+                ('$\mu_{geo}$',scipy.stats.gmean(v[:,0]),),
+                ('$\mu$',np.mean(v[:,1]),),
+                ('median',np.median(v[:,0]),),
+            ])
+
+
+class MatrixBlock:
+    """Representation of a matrix block bounded by fractures"""
+
+    def __init__(self, dfn, pt):
+        """Analyze the dfn to find the block around `pt`"""
+
+        self.pt = pt
+        """[x y z]"""
+
+        #breakpoint()
+        (_bb, _bfx) = dfn.find_block_bounds(pt)
+        #self.bb = _MatrixBlock._dummy_block_bounds(pt, 2.)
+        self.bb = _bb
+        """[x1 x2 y1 y2 z1 z2]"""
+
+        _nbfx = sum(ifx > -1 for ifx in _bfx)
+        self.bfx = sorted(np.fromiter(filter(lambda ifx: ifx>-1, _bfx), count=_nbfx,
+                dtype=np.int32))
+        """Sorted list of indicies of bounding fractures"""
+
+        _length = _bb[1::2]-_bb[::2]
+        self.L = _length
+        self.V = np.product(_length)
+        self.A = 2*sum(np.product(a) for a in combinations(_length, 2))
+        self.Afrac = -1.
+        self.axy = _length[1]/_length[0]
+        self.axz = _length[2]/_length[0]
+
+    def iter_pts(self):
+        _tmp = np.zeros(3)
+        for x in self.bb[:2]:
+            _tmp[0] = x
+            for y in self.bb[2:4]:
+                _tmp[1] = y
+                for z in self.bb[4:]:
+                    _tmp[2] = z
+                    yield _tmp
+
+    @staticmethod
+    def pts_to_tecplot(bllist, fn):
+
+        tecout = ''
+        tecout += f'# {os.path.realpath(__file__)} on {datetime.datetime.now()}\n'
+        tecout += 'VARIABLES="X","Y","Z"\n'
+        tecout += '\n'
+
+        tecout += f'ZONE T="Block initial points" '
+        tecout += f'ZONETYPE=ORDERED I={len(bllist)} DATAPACKING=POINT'
+        tecout += '\n'
+
+        for bl in bllist:
+            tecout += ' '.join(f'{v:8.3f}' for v in bl.pt) + '\n'
+
+        with open(fn, 'w') as fout:
+            print(tecout, file=fout)
+
+        logger.info(f'Wrote sample points as Tecplot data in {fn}')
+
+    @staticmethod
+    def blocks_to_tecplot(bllist, fn):
+        tecout = ''
+        tecout += f'# {os.path.realpath(__file__)} on {datetime.datetime.now()}\n'
+        tecout += 'VARIABLES="X","Y","Z","V","A","X:Y aspect","X:Z aspect","Frac SA"\n'
+        tecout += '\n'
+
+        ne = len(bllist)
+        tecout += f'ZONE T="Block data" '
+        tecout += f'ZONETYPE=FEBRICK '
+        tecout += f'NODES={8*ne} ELEMENTS={ne} DATAPACKING=BLOCK '
+        tecout += 'VARLOCATION=([4-8]=CELLCENTERED)'
+        tecout += '\n'
+
+        # X,Y,Z
+        for i in range(3):
+            tecout += f'# {"XYZ"[i]}\n'
+            for bl in bllist:
+                tecout += ' '.join(f'{v[i]:.3f}' for v in bl.iter_pts()) + '\n'
+        # values
+        for v in ['V', 'A', 'axy', 'axz', 'Afrac',]:
+            tecout += '# '+v+'\n'
+            tecout += '\n'.join(str(getattr(bl, v)) for bl in bllist) + '\n'
+
+        # connectivity
+        for ibl in range(len(bllist)):
+            i = 8*ibl
+            tecout += ' '.join(str(i+ii) for ii in [1,5,7,3,2,6,8,4])+'\n'
+
+
+        with open(fn, 'w') as fout:
+            print(tecout, file=fout)
+
+        logger.info(f'Wrote blocks as Tecplot data in {fn}')
+
+    @staticmethod
+    def _dummy_block_bounds(pt, s):
+        bb = np.zeros(6, dtype=np.single)
+        bb[0] = pt[0]-s
+        bb[1] = pt[0]+s
+        bb[2] = pt[1]-s
+        bb[3] = pt[1]+s
+        bb[4] = pt[2]-s
+        bb[5] = pt[2]+s
+        return bb
