cb431c616c7950c4029962a3bc1f2377068196ee
kmwalton <56695898+kmwalton@users.noreply.github.com>
2023-11-02 09:20:49 -0400

    Minor updates to CLI and logging (console) output.

--- ofrac-blockstats.py
+++ ofrac-blockstats.py
@@ -8,6 +8,8 @@ import shutil
 
 import numpy as np
 
+from time import perf_counter
+
 import ofracs
 from ofracs import numTuple2str as t2s
 from ofrac.matrixblock import MatrixBlockOFracGrid, MatrixBlock
@@ -15,8 +17,7 @@ from ofrac.matrixblock import MatrixBlockOFracGrid, MatrixBlock
 import logging
 logger = logging.getLogger(__file__)
 #logger.setLevel(logging.DEBUG)
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
+_INFO1 = logging.INFO+1
 
 def process_pstats(dfn):
 
@@ -54,7 +55,10 @@ if __name__ == '__main__':
         help='Nudge fractures to increments of this value',
         )
     argp.add_argument('-d', '--sub-domain',
-        metavar='x1 x2 y1 y2 z1 z2',
+        metavar=('x1','x2', 'y1', 'y2', 'z1', 'z2',),
+        default=None,
+        nargs=6,
+        type=float,
         help='Use the given sub-domain and shift its origin to (0,0,0)',
         )
 
@@ -80,11 +84,30 @@ if __name__ == '__main__':
          procedure will be used.''',
         )
 
+    argp.add_argument('-v', '--verbose',
+        default=0,
+        action='count',
+        help='Increase the verbosity. Default 0, maximum 4.',
+        )
 
     argp.add_argument('FILE',
         help='Name of the orthogonal fracture network datafile.')
 
     args = argp.parse_args()
+
+    if args.verbose:
+        liblogger = logging.getLogger('ofrac.matrixblock')
+        if args.verbose > 3:
+            liblogger.setLevel(logging.DEBUG)
+            logger.setLevel(logging.DEBUG)
+        elif args.verbose > 1:
+            liblogger.setLevel(logging.INFO-args.verbose+1)
+            logger.setLevel(logging.INFO-args.verbose+1)
+        elif args.verbose == 1:
+            logger.setLevel(logging.INFO)
+        liblogger.addHandler(logging.StreamHandler())
+        logger.addHandler(logging.StreamHandler())
+
 
     if not args.prefix:
         args.prefix = os.path.splitext(os.path.basename(args.FILE))[0]
@@ -100,10 +123,11 @@ if __name__ == '__main__':
     logger.info(
       f'Read original domain {t2s(dfn.domainOrigin)}-{t2s(dfn.domainSize)}.')
     if args.sub_domain is not None:
-        try:
-            bb = np.fromiter(args.sub_domain.split(), count=6, dtype=float)
-        except:
-            argp.error('Error processing subdomain')
+        #try:
+        #    bb = np.fromiter(args.sub_domain.split(), count=6, dtype=float)
+        #except:
+        #    argp.error('Error processing subdomain')
+        bb = np.array(args.sub_domain)
         dfn.setDomainSize(bb[::2], bb[1::2])
         dfn.translate(-bb[::2])
         logger.info(f'Resized to {t2s(dfn.domainSize,"x","","")}.')
@@ -122,6 +146,9 @@ if __name__ == '__main__':
     else:
         args.nblocks = _tmp
 
+    logger.log(_INFO1, 'Identifying blocks...')
+    _t0 = perf_counter()
+
     # determine sample of matrix blocks
     blocks = None
     if isinstance(args.nblocks, int):
@@ -132,9 +159,11 @@ if __name__ == '__main__':
     else:
         argp.error('Invalid value for --nblocks')
 
+    _t1 = perf_counter()
     logger.info(
       f'Identified {len(blocks)} matrix blocks '
-      +f'in subdomain {t2s(bb[::2])}-{t2s(bb[1::2])}.')
+      +f'in subdomain {t2s(bb[::2])}-{t2s(bb[1::2])} '
+      +f'in {_t1-_t0:.2f} s.')
 
     # apply filter
     if args.block_filter is not None:
